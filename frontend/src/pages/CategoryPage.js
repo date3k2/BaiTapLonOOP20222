73af1f5944883121c80e1b9a9cdb3416cfa004cb
dangptpt <dangptpt@gmail.com>
2023-05-06 11:13:38 +0700

    update AddQuizz, EditQuizz, Category file

--- frontend/src/pages/CategoryPage.js
+++ frontend/src/pages/CategoryPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import OptionsPanel from '../component/OptionsPanel'
 import { Button, Container, NavDropdown, Stack } from 'react-bootstrap'
 import Navbar from 'react-bootstrap/Navbar';
 import Col from 'react-bootstrap/Col';
@@ -7,91 +8,96 @@ import Row from 'react-bootstrap/Row';
 import alert from '../icons/alert.png'
 import questionmark from '../icons/questionmark.png'
 
-export default function AddQuizPage() {
+export default function Category() {
   return (
     <Container className='border p-2'>
-      <Stack direction="horizontal" gap={2}>
-        <NavDropdown
-          style={{ color: 'blue', fontSize: '20px', marginLeft: '25px' }}>
-        </NavDropdown>
-        <Navbar.Text
-          style={{ color: 'red', fontSize: '20px' }}>
-          Add categoty
-        </Navbar.Text>
-      </Stack>
+      <OptionsPanel activeTab={1} />
+      <div style={{ padding: '25px' }}>
+        <Stack direction="horizontal" gap={2}>
+          <NavDropdown
+            style={{ color: 'blue', fontSize: '25px' }}>
+          </NavDropdown>
+          <Navbar.Text
+            style={{ color: 'red', fontSize: '25px' }}>
+            Add categoty
+          </Navbar.Text>
+        </Stack>
 
-      <br />
-      <Form.Group as={Row}>
-        <Form.Label column style={{ fontSize: '20px', marginLeft: '25px' }}>
-          Parent category
-        </Form.Label>
-        <Col>
-          <Stack direction="horizontal" gap={2}>
-            <img src={questionmark} width='13px' height='13px' />
-            <Form.Select style={{width: '350px'}}>
-              <option>Default</option>
-            </Form.Select>
+        <br />
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Parent category
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={questionmark} width='13px' height='13px' />
+              <Form.Select style={{ width: '350px' }}>
+                <option>Default</option>
+              </Form.Select>
+            </Stack>
+          </Col>
+        </Form.Group>
+
+        <br />
+
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Name
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={alert} width='13px' height='13px' />
+              <Form.Control type="text" style={{ width: '480px' }} />
+            </Stack>
+          </Col>
+        </Form.Group>
+
+        <br />
+
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Category info
+          </Form.Label>
+          <Col >
+            <Stack direction="horizontal" gap={2}>
+              <img src={alert} width='13px' height='13px' style={{ marginBottom: '260px' }} />
+              <Form.Control type="text" as="textarea" style={{ height: '300px' }}
+              />
+            </Stack>
+          </Col>
+        </Form.Group>
+
+        <br />
+
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            ID number
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={questionmark} width='13px' height='13px' />
+              <Form.Control type="text" style={{ width: '100px' }} />
+            </Stack>
+          </Col>
+        </Form.Group>
+        <br />
+        <div style={{ display: "flex", justifyContent: 'center' }}>
+          <Button
+            variant='danger'
+            href='/category'
+          >
+            ADD CATEGORY
+          </Button>
+        </div>
+        <br />
+        <div style={{ display: "flex", justifyContent: 'center' }}>
+          <Stack direction="horizontal" gap={1}>
+            <p > There are required fields in this form marked</p>
+            <img src={alert} width='13px' height='13px' style={{ marginBottom: '13px' }} />
           </Stack>
-        </Col>
-      </Form.Group>
+        </div>
 
-      <br />
-
-      <Form.Group as={Row}>
-        <Form.Label column style={{ fontSize: '20px', marginLeft: '25px' }}>
-          Name
-        </Form.Label>
-        <Col>
-          <Stack direction="horizontal" gap={2}>
-            <img src={alert} width='13px' height='13px' />
-            <Form.Control type="text" style={{ width: '480px' }} />
-          </Stack>
-        </Col>
-      </Form.Group>
-
-      <br />
-
-      <Form.Group as={Row}>
-        <Form.Label column style={{ fontSize: '20px', marginLeft: '25px' }}>
-          Category info
-        </Form.Label>
-        <Col >
-          <Stack direction="horizontal" gap={2}>
-            <img src={alert} width='13px' height='13px' style={{ marginBottom: '260px' }} />
-            <Form.Control type="text" as="textarea" style={{ height: '300px' }}
-            />
-          </Stack>
-        </Col>
-      </Form.Group>
-
-      <br />
-
-      <Form.Group as={Row}>
-        <Form.Label column style={{ fontSize: '20px', marginLeft: '25px' }}>
-          ID number
-        </Form.Label>
-        <Col>
-          <Stack direction="horizontal" gap={2}>
-            <img src={questionmark} width='13px' height='13px' />
-            <Form.Control type="text" style={{ width: '100px' }} />
-          </Stack>
-        </Col>
-      </Form.Group>
-      <br />
-      <Button
-        variant='danger'
-        style={{ marginLeft: '660px' }}
-        href='/category'
-      >
-        ADD CATEGORY
-      </Button>
-      <br />
-      <br />
-      <Stack direction="horizontal" gap={1}>
-        <p style={{ marginLeft: '550px' }}> There are required fields in this form marked</p>
-        <img src={alert} width='13px' height='13px' style={{ marginBottom: '13px' }} />
-      </Stack>
-
-    </Container>
+      </div>
+    </Container >
   )
 }
--- frontend/src/pages/Quiz/AddQuizPage.js
+++ frontend/src/pages/Quiz/AddQuizPage.js
@@ -1,7 +1,178 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Container, FormCheck, FormControl, NavDropdown, Stack } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import alert from '../../icons/alert.png'
+import exam from '../../icons/exam.png'
+import questionmark from '../../icons/questionmark.png'
 
-export default function EditQuizPage() {
+
+export default function AddQuizPage() {
   return (
-    <div>AddQuizPage</div>
+    <Container className='border p-2'>
+      <Stack direction="horizontal" gap={2}>
+        <img src={exam} width='15px' height='17px' style={{ marginBottom: '10px' }} />
+        <p
+          style={{ color: 'red', fontSize: '30px' }}>
+          Adding a new Quizz
+        </p>
+        <img src={questionmark} width='15px' height='15px' style={{ marginBottom: '10px' }} />
+      </Stack>
+
+      <div style={{ padding: '25px' }}>
+        <Stack direction="horizontal" gap={2}>
+          <NavDropdown
+            style={{ color: 'blue', fontSize: '25px' }}>
+          </NavDropdown>
+          <Navbar.Text
+            style={{ color: 'red', fontSize: '25px' }}>
+            General
+          </Navbar.Text>
+        </Stack>
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Name
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={alert} width='13px' height='13px' />
+              <Form.Control type='text' style={{ width: '500px' }} />
+            </Stack>
+          </Col>
+        </Form.Group>
+
+        <br />
+
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Description
+          </Form.Label>
+          <Col style={{ marginLeft: '42px' }} >
+            <Form.Control type="text" as="textarea" style={{ height: '300px' }} />
+
+            <Stack direction="horizontal" gap={2}>
+              <Form.Check
+                type='checkbox'
+                label='Display description on course page'
+              />
+              <img src={questionmark} width='13px' height='13px' />
+            </Stack>
+          </Col>
+        </Form.Group>
+
+        <hr />
+        <Stack direction="horizontal" gap={2}>
+          <NavDropdown
+            style={{ color: 'blue', fontSize: '25px' }}>
+          </NavDropdown>
+          <Navbar.Text
+            style={{ color: 'red', fontSize: '25px' }}>
+            Timing
+          </Navbar.Text>
+        </Stack>
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Open the quizz
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={questionmark} width='13px' height='13px' />
+              <Form.Select style={{ width: '50px' }}>
+                {
+                }
+              </Form.Select >
+              <Form.Select style={{ width: '150px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '100px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '50px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '50px' }}>
+
+              </Form.Select>
+              <Form.Check
+                type='checkbox'
+                label='Enable'
+              />
+            </Stack>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Close the quizz
+          </Form.Label>
+          <Col style={{ marginLeft: '42px' }}>
+            <Stack direction="horizontal" gap={2}>
+              <Form.Select style={{ width: '50px' }}>
+                {
+                }
+              </Form.Select >
+              <Form.Select style={{ width: '150px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '100px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '50px' }}>
+
+              </Form.Select>
+              <Form.Select style={{ width: '50px' }}>
+
+              </Form.Select>
+              <Form.Check
+                type='checkbox'
+                label='Enable'
+              />
+            </Stack>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            Time limit
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={questionmark} width='13px' height='13px' />
+              <FormControl type='text' style={{ width: '100px' }} />
+              <Form.Select style={{ width: '100px' }}>
+                <option value={1}>
+                  minutes
+                </option>
+                <option value={2}>
+                  hours
+                </option>
+              </Form.Select>
+              <Form.Check
+                type='checkbox'
+                label='Enable'
+              />
+            </Stack>
+          </Col>
+        </Form.Group>
+        <Form.Group as={Row}>
+          <Form.Label column style={{ fontSize: '20px' }}>
+            When time expires
+          </Form.Label>
+          <Col>
+            <Stack direction="horizontal" gap={2}>
+              <img src={questionmark} width='13px' height='13px' />
+              <Form.Select style={{ width: '300px' }}>
+                <option>Opens attempts are submitted automatic</option>
+              </Form.Select>
+            </Stack>
+          </Col>
+        </Form.Group>
+      </div>
+      <br />
+      <div style={{ display: "flex", justifyContent: 'center' }}>
+        <Button variant="danger">Create </Button>
+        <Button variant="primary" style={{marginLeft:'10px'}}>Cancel </Button>
+      </div>
+    </Container >
   )
 }
