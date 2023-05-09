ddee4db28ba97d720af9bcd7aa375efcb184f176
Mon62 <Nam.NV210618@sis.hust.edu.vn>
2023-05-09 11:09:15 +0700

    add QuizPagee, EditQuizPage file

--- frontend/src/pages/Quiz/EditQuizPage.js
+++ frontend/src/pages/Quiz/EditQuizPage.js
@@ -1,7 +1,134 @@
-import React from 'react'
+import React from "react";
+import { Container, Col, Stack, Button, Dropdown } from "react-bootstrap";
+import { BsFillPencilFill } from "react-icons/bs";
+import { ImPlus } from "react-icons/im";
+import questionmark from "../../icons/questionmark.png";
+import { useState } from "react";
 
-export default function EditQuizPage() {
+function HandleHighlight(props) {
+  const [isHover, setIsHover] = useState(null);
+  const isHovered = () => {
+    return (props.id === isHover);
+  };
   return (
-    <div>EditQuizPage</div>
-  )
+    <Stack
+      direction="horizontal"
+      style={{ backgroundColor: isHovered() ? "lightblue" : "white" }}
+      onMouseOver={() => {
+        setIsHover(props.id)}
+      }
+      onMouseOut={() => setIsHover(null)}
+
+    >
+      <Col className="d-flex justify-content-center" xs={3}>
+        <ImPlus size={15} color="#0081C9" />
+      </Col>
+      <Col> {props.name} </Col>
+    </Stack>
+  );
+}
+export default function EditQuizPage() {
+  const [isChecked, setIsChecked] = useState(false);
+  const handleOnClick = () => {
+    setIsChecked(!isChecked);
+  };
+
+  const items = ["a new question", "from question bank", "a random question"];
+
+  return (
+    <Container className="border p-2">
+      <Stack direction="horizontal" gap={1}>
+        <div style={{ color: "red", fontSize: "35px" }}>
+          Editing quiz: Thi giữa kì 2 môn Công nghệ
+        </div>
+        <img src={questionmark} style={{ width: "15px", height: "15px" }} />
+      </Stack>
+
+      <Stack className="my-1" direction="horizontal">
+        <Col style={{ fontSize: "16px" }}>Question: 0 | This quiz is open</Col>
+        <div className="d-flex gap-1 align-items-center">
+          <Col>Maximum grade</Col>
+          <input
+            type="text"
+            className="form-control"
+            style={{ width: "60px" }}
+          />
+          <Button
+            type="button"
+            style={{ backgroundColor: "#0081C9" }}
+            href="/previewquiz/editquiz"
+          >
+            SAVE
+          </Button>
+        </div>
+      </Stack>
+
+      <Stack direction="horizontal">
+        <div className="d-flex gap-1 align-items-center">
+          <Button type="button" style={{ backgroundColor: "#0081C9" }}>
+            REPAGINATE
+          </Button>
+          <Button type="button" style={{ backgroundColor: "#0081C9" }}>
+            SELECT MULTIPLEITEMS
+          </Button>
+        </div>
+        <Col
+          className="d-flex justify-content-end"
+          style={{ fontSize: "16px" }}
+        >
+          Total of marks: {1}
+        </Col>
+      </Stack>
+
+      <Container className="mt-3" style={{ background: "#F2EFEA" }}>
+        <Stack direction="horizontal" gap={1}>
+          <BsFillPencilFill size={12} color="0081C9" />
+          <Col className="d-flex justify-content-end">
+            <Button
+              variant={isChecked ? "success" : "outline-secondary"}
+              onClick={handleOnClick}
+              style={{
+                width: "15px",
+                height: "15px",
+                borderRadius: "5px",
+                padding: "0",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              {isChecked && "\u2713"}
+            </Button>
+          </Col>
+          <div>Shuffle</div>
+          <img src={questionmark} style={{ width: "15px", height: "15px" }} />
+        </Stack>
+
+        <div className="d-flex justify-content-end">
+          <Dropdown className="pt-2" drop="down" align={"end"}>
+            <Dropdown.Toggle className="d-flex align-items-center">
+              <div style={{ margin: "5px" }}>Add</div>
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Container className="d-flex" style={{ width: "250px" }}>
+                <Stack>
+                  {items.map((item, index) => (
+                    <HandleHighlight name={item} id={index}/>
+                  ))}
+                </Stack>
+              </Container>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      </Container>
+
+      <div className="col-md-1 mx-auto" style={{ marginTop: "250px" }}>
+        <select style={{ width: "150px" }}>
+          <option value="" selected="">
+            Jump to...
+          </option>
+        </select>
+      </div>
+    </Container>
+  );
 }
--- frontend/src/pages/Quiz/QuizPage.js
+++ frontend/src/pages/Quiz/QuizPage.js
@@ -1,11 +1,84 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { Container, Stack, Col, Row, Button } from "react-bootstrap";
+import { MdArrowDropDownCircle } from "react-icons/md";
 
-function QuizPage() {
+export default function PreviewQuiz() {
+
+  const path = window.location.pathname;
 
   return (
-    <div>QuizPage</div>
-  )
-}
+    <Container className="border pl-4">
+      <Row>
+        <Col style={{ color: "red", fontSize: "25px" }}>
+          Thi giữa kì 2 môn Công nghệ
+        </Col>
+        <Col className="d-flex justify-content-end">
+          <a href={`${path}/edit`}>
+            <MdArrowDropDownCircle
+              size={40}
+              color="#0081C9"
+            />
+          </a>
+        </Col>
+      </Row>
+      <Row className="mt-4">
+        <div
+          className="container"
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <p>Time limit: 1 hour</p>
+          <p>Grading method: Last grade</p>
+        </div>
+      </Row>
+      <Row>
+        <div style={{ color: "red", fontSize: "20px" }}>
+          Summary of your previous attempt
+        </div>
+      </Row>
+      <Row className="mt-2">
+        <Stack>
+          <Stack
+            direction="horizontal"
+            className="border p-2"
+            style={{ color: "white", backgroundColor: "#0081C9" }}
+          >
+            <Col style={{ textAlign: "center" }}> Attempt </Col>
+            <Col> State </Col>
+          </Stack>
+          <Stack
+            direction="horizontal"
+            className="border p-2"
+            style={{ color: "black", backgroundColor: "#F1F6F5" }}
+          >
+            <Col style={{ textAlign: "center" }}> Preview </Col>
+            <Col> Never submitted </Col>
+          </Stack>
+        </Stack>
+      </Row>
 
-export default QuizPage
+      <Row className="my-5">
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <Button
+            type="button"
+            style={{ backgroundColor: "#0081C9" }}
+            href="/previewquiz"
+          >
+            PREVIEW QUIZ NOW
+          </Button>
+        </div>
+      </Row>
+      <div className="col-md-1 mx-auto ">
+        <select>
+          <option value="" selected="">
+            Jump to...
+          </option>
+        </select>
+      </div>
+    </Container>
+  );
+}
