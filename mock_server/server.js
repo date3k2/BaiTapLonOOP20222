53c1ca1dd9f45e0f0a85c582d995fed04abc6ba2
huulamthcsyl <huulamthcsyl@gmail.com>
2023-06-02 12:57:04 +0700

    Update mock server

--- mock_server/server.js
+++ mock_server/server.js
@@ -8,15 +8,29 @@ app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
 
+// Quiz
+
 app.get('/quiz', (req, res) => {
     data = require('./data/quiz.json');
     res.json(data);
 });
 
+app.post('/quiz', (req, res) => {
+    res.json("Add new quiz successfully!");
+});
+
+// Category
+
 app.get('/categories', (req, res) => {
     data = require('./data/category.json');
     res.json(data);
 });
+
+app.post('/category', (req, res) => {
+    res.json("Add new category successfully!"); 
+ });
+
+ // Question
 
 app.get('/questions', (req, res) => {
     let category = req.query.category;
@@ -39,11 +53,20 @@ app.get('/question', (req, res) => {
     // res.json(data)
 })
 
+app.post('/question', (req, res) => {
+    res.json("Add new question successfully!");
+});
+
+app.put('/question', (req, res) => {
+    res.json("Change question successfully!");
+});
+
+// Import 
+
 app.post('/import', (req, res) => {
     let message = {
         "type": "success",
         "message": "Upload successfully!"
     };
-    res.json(message)
+    res.json(message);
 })
-
