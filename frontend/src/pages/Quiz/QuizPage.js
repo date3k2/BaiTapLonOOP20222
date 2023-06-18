2702000aba71ee0ce40af222abd0b7a517454ac7
Mon62 <Nam.NV210618@sis.hust.edu.vn>
2023-06-18 23:05:52 +0700

    small upadte Edit Quiz

--- frontend/src/pages/Quiz/QuizPage.js
+++ frontend/src/pages/Quiz/QuizPage.js
@@ -3,38 +3,26 @@ import { Container, Stack, Col, Row, Button } from "react-bootstrap";
 import { MdArrowDropDownCircle } from "react-icons/md";
 import PreviewQuizModal from "./PreviewQuizModal";
 import apiServices from "../../services/apiServices";
+import { useParams } from "react-router-dom";
 
 export default function PreviewQuiz() {
-  //  const path = window.location.pathname;
-  const [QuizList, setQuizList] = useState([]);
+    
+  const data = useParams();
+   const [QuizTimelimit, setQuizTimelimit] = useState([]);
 
-  useEffect(() => {
-    apiServices
-      .getQuiz()
-      .then((res) => setQuizList(res.data))
-      .catch((err) => console.log(err));
-  }, []);
-
+    useEffect(() => {
+      apiServices.getQuiz(data.quizName)
+        .then((res) => setQuizTimelimit(res.data.timeLimitInSeconds))
+        .catch((err) => console.log(err));
+    }, []);
   const [openModal, setOpenModal] = useState(false);
-  const [quizName, setQuizName] = useState("");
-
-  useEffect(() => {
-    setQuizName(decodeURIComponent(window.location.pathname.slice(1)));
-  }, []);
-
-  const timeLimit = QuizList.reduce((acc, quiz) => {
-    if (quiz.quizName === quizName) {
-      return quiz.timeLimit;
-    }
-    return acc;
-  }, null);
 
   return (
     <Container className="border pl-4">
       <Row>
-        <Col style={{ color: "red", fontSize: "25px" }}>{quizName}</Col>
+        <Col style={{ color: "red", fontSize: "25px" }}>{data.quizName}</Col>
         <Col className="d-flex justify-content-end">
-          <a href={`${quizName}/edit`}>
+          <a href={`${data.quizName}/edit`}>
             <MdArrowDropDownCircle size={40} color="#0081C9" />
           </a>
         </Col>
@@ -49,7 +37,7 @@ export default function PreviewQuiz() {
             justifyContent: "center",
           }}
         >
-          <p>Time limit: {timeLimit} minutes</p>
+          <p>Time limit: {QuizTimelimit} minutes</p>
           <p>Grading method: Last grade</p>
         </div>
       </Row>
@@ -88,7 +76,7 @@ export default function PreviewQuiz() {
           >
             PREVIEW QUIZ NOW
           </Button>
-          <PreviewQuizModal open={openModal} onClose={() => setOpenModal(false)} timeLimit = {timeLimit}/>
+          <PreviewQuizModal open={openModal} onClose={() => setOpenModal(false)} timeLimit = {QuizTimelimit}/>
         </div>
       </Row>
     </Container>
